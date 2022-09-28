f1d36037b0dc294530a0d5349837ec8586290921
Fe-56 <limfuoen1999@gmail.com>
2022-09-28 23:01:55 +0800

    Update

--- src/BarChart.js
+++ src/BarChart.js
@@ -5,39 +5,45 @@ const BarChart = (props) => {
     const d3BarChart = React.useRef();
     const dates = [];
     const gdp = [];
-    const height = "100%";
-    const width = "100%";
+    const height = 800;
+    const width = 1000;
 
     useEffect(() => {
         fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json")
         .then(res => res.json())
         .then((result) => {
+            var parseTime = d3.timeParse("%Y-%m-%d");
             for (let i = 0; i < result.data.length; i++){
-                dates.push(result.data[i][0]);
+                dates.push(parseTime(result.data[i][0]));
                 gdp.push(result.data[i][1]);
             }
 
-            // const xAxis = d3.scaleTime()
-            //                 .domain([dates]);
+            var xScale = d3.scaleTime()
+                        .domain(d3.extent(dates))
+                        .range([0, width]);
+            const xAxis = d3.axisBottom().scale(xScale);
+            const yScale = d3.scaleLinear()
+                            .domain([0, d3.max(gdp)])
+                            .range([0, height]);
+            const yAxis = d3.axisLeft().scale(yScale);
 
-            // const yAxis = d3.scaleLinear()
-            //                 .domain([Math.min(gdp), Math.max(gdp)])
-            //                 .range
-
-            const svg = d3.select(d3BarChart.current)
-                            .attr("width", width)
-                            .attr("height", height)
-                            .append("rect")
-                            .attr("width", "100%")
-                            .attr("height", "100%")
-                            .attr("fill", "red")
+            var svg = d3.select(d3BarChart.current)
+                        .attr("width", width)
+                        .attr("height", height)
+                        .append("g")
+                        .attr('id', 'x-axis')
+                        .attr("transform", "translate(60, 500)")
+                        .call(xAxis.ticks(d3.timeYear.every(5)))
+                        .append("g")
+                        .attr('id', 'y-axis')
+                        .attr("transform", "translate(0, -50)")
+                        .call(yAxis);
         })
     }, []);
 
     return (
         <div>
             <svg ref={d3BarChart} />
-            <p></p>
         </div>
     )
 }
